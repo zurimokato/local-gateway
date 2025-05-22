21284b763c0066db9f98cf7477a3106c582d194b
zurimokato <noj2304@gmail.com>
2025-05-22 15:04:19 -0500

    se protegen todos los endpoints via programatica

--- src/main/java/com/local/gateway/config/GateWayConfig.java
+++ src/main/java/com/local/gateway/config/GateWayConfig.java
@@ -19,11 +19,26 @@ public class GateWayConfig {
     public RouteLocator routes(RouteLocatorBuilder builder, AuthenticationFilter authenticationFilter) {
         return builder
                 .routes()
-                .route(r -> r.path("/local/api/v1/products/**").uri("lb://ms-product"))
-                .route(r -> r.path("/local/api/v1/stores/**").uri("lb://ms-store"))
-                .route(r -> r.path("/local/api/v1/clients/**").uri("lb://ms-client"))
-                .route(r->r.path("/local/api/v1/orders/**").uri("lb://ms-order"))
-                .route(r->r.path("/local/api/v1/notifications/**").uri("lb://ms-notification"))
+                .route(r -> r.path("/local/api/v1/products/**")
+                        .filters(f->
+                                f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
+                        .uri("lb://ms-product"))
+                .route(r -> r.path("/local/api/v1/stores/**")
+                        .filters(f->
+                                f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
+                        .uri("lb://ms-store"))
+                .route(r -> r.path("/local/api/v1/clients/**")
+                        .filters(f->
+                                f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
+                        .uri("lb://ms-client"))
+                .route(r->r.path("/local/api/v1/orders/**")
+                        .filters(f->
+                                f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
+                        .uri("lb://ms-order"))
+                .route(r->r.path("/local/api/v1/notifications/**")
+                        .filters(f->
+                                f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
+                        .uri("lb://ms-notification"))
                 .route(r->r.path("/local/api/v1/menus/**")
                         .filters(f->
                                 f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
